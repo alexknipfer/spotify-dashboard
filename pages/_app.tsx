a2df7e50c71532b49f218058b187988248b61c7d
Alex <alexanderknipfer@gmail.com>
2021-02-26 21:06:51 -0500

    Remove with skeleton

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import AuthRedirect from '@/components/AuthRedirect';
 import { Provider } from 'next-auth/client';
-import WithSkeleton from '@/components/WithSkeleton';
 import { CustomAppProps } from '@/interfaces/CustomPage';
 import { pick } from '@/lib/utils';
 import { NextPage } from 'next';
@@ -9,8 +8,6 @@ import { SWRConfig } from 'swr';
 import '../styles/globals.css';
 
 const App: NextPage<CustomAppProps> = ({ Component, pageProps }) => {
-  const skeletonLoader = Component.skeletonLoader;
-
   const authRedirect = pick(
     Component,
     'redirectAuthenticatedTo',
@@ -21,16 +18,14 @@ const App: NextPage<CustomAppProps> = ({ Component, pageProps }) => {
   return (
     <Provider session={pageProps.session}>
       <AuthRedirect {...authRedirect}>
-        <WithSkeleton skeletonLoader={skeletonLoader}>
-          <SWRConfig
-            value={{
-              fetcher: (resource, init) =>
-                fetch(resource, init).then((res) => res.json()),
-            }}
-          >
-            <Component {...pageProps} />
-          </SWRConfig>
-        </WithSkeleton>
+        <SWRConfig
+          value={{
+            fetcher: (resource, init) =>
+              fetch(resource, init).then((res) => res.json()),
+          }}
+        >
+          <Component {...pageProps} />
+        </SWRConfig>
       </AuthRedirect>
     </Provider>
   );
