20d44da54862fd7c6ea28008627e5b401ffee318
Alex <alexanderknipfer@gmail.com>
2021-02-27 17:58:54 -0500

    Add basic layout for artists

--- components/dashboard/Dashboard.tsx
+++ components/dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
-import { SpotifyProfile } from '@/interfaces/Spotify';
+import { SpotifyProfile, SpotifyTopArtists } from '@/interfaces/Spotify';
+import { Fragment } from 'react';
 import useSWR from 'swr';
 
 import DashboardHeader from './DashboardHeader';
@@ -9,8 +10,23 @@ interface Props {
 
 const Dashboard: React.FC<Props> = () => {
   const { data } = useSWR<SpotifyProfile>('/api/get-profile');
+  const { data: topArtists } = useSWR<SpotifyTopArtists>('/api/top-artists');
 
-  return <DashboardHeader isLoading={!data} profile={data} />;
+  return (
+    <Fragment>
+      <DashboardHeader isLoading={!data} profile={data} />
+      <section className="grid grid-cols-2 gap-5">
+        <div>
+          <h2 className="text-xl font-bold">Top Artists of All Time</h2>
+          {topArtists?.items.map((artistDetails) => (
+            <div key={artistDetails.id} className="mb-2">
+              {artistDetails.name}
+            </div>
+          ))}
+        </div>
+      </section>
+    </Fragment>
+  );
 };
 
 export default Dashboard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import { CustomPage } from '@/interfaces/CustomPage';
 import Dashboard from '@/components/dashboard/Dashboard';
 import withAuthentication from 'hoc/WithAuthentication';
+import DashboardLayout from '@/layouts/DashboardLayout';
 
 const Home: CustomPage = () => {
-  return <Dashboard />;
+  return (
+    <DashboardLayout>
+      <Dashboard />
+    </DashboardLayout>
+  );
 };
 
 export default withAuthentication(Home);
