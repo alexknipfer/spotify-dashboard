f21ee3b6be207c3040da434c5afb7866018fe64f
Alex <alexanderknipfer@gmail.com>
2021-02-28 14:42:59 -0500

    Add navbar

--- layouts/DashboardLayout.tsx
+++ layouts/DashboardLayout.tsx
@@ -1,10 +1,49 @@
 import { Fragment } from 'react';
+import Link from 'next/link';
+import classnames from 'classnames';
+import { useRouter } from 'next/router';
+
+import ProfileIcon from '../public/static/icons/profile_icon.svg';
+import SpotifyLogo from '../public/static/icons/spotify_logo.svg';
+
+const navItems = [
+  {
+    name: 'Profile',
+    href: '/',
+    icon: <ProfileIcon className="fill-current" />,
+  },
+];
 
 const DashboardLayout: React.FC = ({ children }) => {
+  const router = useRouter();
+
   return (
     <Fragment>
-      <nav className="sticky top-0 z-20 h-24 bg-black w-full shadow-lg" />
-      <main className="px-5 md:px-28 py-10 max-w-screen-2xl mx-auto min-h-screen">
+      <nav className="fixed bottom-0 md:sticky md:top-0 z-20 h-16 md:h-20 bg-black w-full shadow-lg px-0 md:px-16 lg:px-28 flex items-center justify-between">
+        <div className="hidden md:block w-36 py-4">
+          <SpotifyLogo />
+        </div>
+        <div className="flex items-center h-full">
+          {navItems.map((navItem) => (
+            <Link key={navItem.href} href={navItem.href}>
+              <a
+                className={classnames(
+                  'text-xs flex items-center flex-col justify-center h-full px-8 hover:bg-spotify-gray hover:text-white',
+                  {
+                    'text-white bg-spotify-gray border-b-2 border-spotify-green':
+                      router.pathname === navItem.href,
+                    'text-gray-400': router.pathname !== navItem.href,
+                  },
+                )}
+              >
+                <div className="h-5 w-5 md:h-6 md:w-6 mb-1">{navItem.icon}</div>
+                <span>{navItem.name}</span>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </nav>
+      <main className="px-5 md:px-16 lg:px-28 py-10 max-w-screen-2xl mx-auto min-h-screen">
         {children}
       </main>
     </Fragment>
