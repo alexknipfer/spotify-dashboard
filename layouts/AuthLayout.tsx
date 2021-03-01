cd57572e3f387c161215a11479dd11c9357ce394
Alex <alexanderknipfer@gmail.com>
2021-03-01 12:05:51 -0500

    Fix body scroll

--- layouts/AuthLayout.tsx
+++ layouts/AuthLayout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout: React.FC = ({ children }) => {
   return (
-    <main className="flex flex-col justify-center h-full items-center max-w-screen-2xl mx-auto">
+    <main className="flex flex-col justify-center h-screen items-center max-w-screen-2xl mx-auto">
       {children}
     </main>
   );
