b16c9ad9d41eb3f0b37636087f321d40387e12fa
Alex <alexanderknipfer@gmail.com>
2021-03-01 15:14:11 -0500

    Add external link support to button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,10 @@
+import classnames from 'classnames';
+
 interface CustomProps {
   variant: 'primary';
+  buttonSize?: 'small' | 'medium' | 'large';
   type?: 'button' | 'submit';
+  href?: string;
 }
 
 type Props = CustomProps & React.HTMLProps<HTMLButtonElement>;
@@ -9,8 +13,26 @@ const Button: React.FC<Props> = ({
   variant,
   type = 'button',
   children,
+  href,
+  buttonSize = 'medium',
   ...props
 }) => {
+  const className = classnames(
+    'bg-spotify-green hover:bg-spotify-light-green transition duration-200 text-white rounded-full uppercase tracking-widest font-bold',
+    {
+      'py-3 px-6 text-xs': buttonSize === 'small',
+      'py-4 px-10 text-base': buttonSize === 'medium',
+    },
+  );
+
+  if (href) {
+    return (
+      <a href={href} className={className} target="_blank" rel="noreferrer">
+        {children}
+      </a>
+    );
+  }
+
   return (
     <button
       type={type}
