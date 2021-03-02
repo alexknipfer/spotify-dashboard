d3e976ad2f040ad4c15330720c9a75bf7cd1a521
Alex <alexanderknipfer@gmail.com>
2021-03-02 13:07:47 -0500

    Add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link
             rel="preload"
@@ -31,6 +31,33 @@ class MyDocument extends Document {
             type="font/woff2"
             crossOrigin="anonymous"
           />
+          <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/static/favicons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="rgb(24, 24, 24)" />
+          <meta charSet="utf-8" />
         </Head>
         <body className="bg-spotify-gray text-white">
           <Main />
