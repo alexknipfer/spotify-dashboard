acd5f802f2006cc3a6822c54b198dafd5fa24309
alexknipfer <alexanderknipfer@gmail.com>
2021-03-04 21:36:45 -0500

    Move meta to dashboard layout

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -58,50 +58,6 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="rgb(24, 24, 24)" />
           <meta charSet="utf-8" />
-          <meta
-            name="description"
-            content="Find your most recent spotify music, top artists, top songs, and more!"
-          />
-          <meta
-            name="og:title"
-            content="Spotify Dashboard - The place to find your Spotify statistics."
-          />
-          <meta
-            property="og:url"
-            content={`https://spotify-dashboard-alexknipfer.vercel.app`}
-          />
-          <link
-            rel="canonical"
-            href={`https://spotify-dashboard-alexknipfer.vercel.app`}
-          />
-          <meta property="og:type" content="website" />
-          <meta property="og:site_name" content="Spotify Dashboard" />
-          <meta
-            property="og:description"
-            content="Find your most recent spotify music, top artists, top songs, and more!"
-          />
-          <meta
-            property="og:title"
-            content="Spotify Dashboard - The place to find your Spotify statistics."
-          />
-          <meta
-            property="og:image"
-            content="https://spotify-dashboard-alexknipfer.vercel.app/static/images/spotify_dashboard_card.jpg"
-          />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@knipferalex" />
-          <meta
-            name="twitter:title"
-            content="Spotify Dashboard - The place to find your Spotify statistics."
-          />
-          <meta
-            name="twitter:description"
-            content="Find your most recent spotify music, top artists, top songs, and more!"
-          />
-          <meta
-            name="twitter:image"
-            content="https://spotify-dashboard-alexknipfer.vercel.app/static/images/spotify_dashboard_card.jpg"
-          />
         </Head>
         <body className="bg-spotify-gray text-white">
           <Main />
