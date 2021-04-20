1f6264c04a5a991a381b9324da99dc4ee77a52a2
Alex <alexanderknipfer@gmail.com>
2021-04-20 11:26:06 -0400

    Destroy chart during cleanup

--- components/AudioFeaturesChart.tsx
+++ components/AudioFeaturesChart.tsx
@@ -1,6 +1,7 @@
 import { SpotifyAudioFeatures } from '@/models/Spotify';
 import { Fragment, useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
+import { Chart as ChartModel } from 'chart.js';
 
 const keys = [
   'acousticness',
@@ -17,13 +18,14 @@ interface Props {
 
 const AudioFeaturesChart: React.FC<Props> = ({ audioFeatures }) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const chartRef = useRef<ChartModel | null>(null);
 
   useEffect(() => {
     if (!canvasRef.current) {
       return;
     }
 
-    new Chart(canvasRef.current, {
+    chartRef.current = new Chart(canvasRef.current, {
       type: 'bar',
       options: {
         maintainAspectRatio: false,
@@ -79,13 +81,17 @@ const AudioFeaturesChart: React.FC<Props> = ({ audioFeatures }) => {
         ],
       },
     });
+
+    return () => {
+      chartRef.current && chartRef.current.destroy();
+    };
   }, [audioFeatures]);
 
   return (
     <Fragment>
-      <div className="text-xl text-white font-bold text-center mb-5">
+      <h3 className="text-xl text-white font-bold text-center mb-5">
         Track Audio Features
-      </div>
+      </h3>
       <div className="w-full relative h-96">
         <canvas id="chart" ref={canvasRef} width="500" height="500" />
       </div>
