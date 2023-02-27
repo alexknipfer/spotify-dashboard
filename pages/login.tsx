f4ddd41e612faa5c60f06fb7eb199cfe3b632e14
Alexander Knipfer <alexanderknipfer@gmail.com>
2023-02-27 00:54:11 -0500

    Add next auth middleware for protected routes

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,41 +1,20 @@
-import { signIn, useSession } from 'next-auth/react';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { signIn } from 'next-auth/react';
 
 import Button from '@/components/Button';
 import Heading from '@/components/Heading';
-import { CustomPage } from '@/models/CustomPage';
-import {
-  NoPageFlicker,
-  NO_PAGE_FLICKER_CLASSNAME,
-} from '@/components/NoPageFlicker';
 
-const Login: CustomPage = () => {
-  const { status } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (status !== 'authenticated' && status !== 'loading') {
-      document.documentElement.classList.add(NO_PAGE_FLICKER_CLASSNAME);
-    } else {
-      router.replace('/login');
-    }
-  }, [status, router]);
-
+export default function Login() {
   return (
     <>
-      <NoPageFlicker />
       <Heading level="h1" className="mb-10">
         Spotify Profile
       </Heading>
       <Button
         variant="primary"
-        onClick={() => signIn('spotify', { callbackUrl: '/' })}
+        onClick={() => signIn('spotify', { callbackUrl: '/dashboard' })}
       >
         Log in to Spotify
       </Button>
     </>
   );
-};
-
-export default Login;
+}
