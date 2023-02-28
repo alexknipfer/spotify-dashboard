32a93f7e75b5834059aaae54f5e609a2e8a7db29
Alexander Knipfer <alexanderknipfer@gmail.com>
2023-02-27 20:35:53 -0500

    Add swr config to providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,12 +2,16 @@
 
 import { SessionProvider } from 'next-auth/react';
 
-// import CustomSWRConfig from '@/components/CustomSWRConfig';
+import CustomSWRConfig from '@/components/CustomSWRConfig';
 
 interface Props {
   children: React.ReactNode;
 }
 
 export function Providers({ children }: Props) {
-  return <SessionProvider refetchInterval={5 * 60}>{children}</SessionProvider>;
+  return (
+    <SessionProvider refetchInterval={5 * 60}>
+      <CustomSWRConfig>{children}</CustomSWRConfig>
+    </SessionProvider>
+  );
 }
