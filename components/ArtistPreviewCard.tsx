d819c3024d179682d257ce81bbea92a3fc9b1c41
Alexander Knipfer <alexanderknipfer@gmail.com>
2023-03-06 21:32:53 -0500

    Fix client component errors and image styling

--- components/ArtistPreviewCard.tsx
+++ components/ArtistPreviewCard.tsx
@@ -13,15 +13,19 @@ interface Props {
 
 export function ArtistPreviewCard({ name, images, href }: Props) {
   return (
-    <div className="text-center">
+    <div className="flex flex-col items-center">
       {images?.length ? (
-        <Image
-          src={images[0].url}
-          width={208}
-          height={208}
-          className="rounded-full"
-          alt=""
-        />
+        <div className="relative w-40 h-40 md:h-[208px] md:w-[208px]">
+          <Image
+            src={images[0].url}
+            className="rounded-full object-cover"
+            alt={`Image of ${name}`}
+            fill
+            sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+          />
+        </div>
       ) : (
         <div className="flex justify-center items-center border border-white rounded-full p-7 w-52 h-52">
           <ProfileIcon fill="#fff" width="100%" height="100%" />
@@ -39,15 +43,8 @@ export function ArtistPreviewCard({ name, images, href }: Props) {
 export function ArtistPreviewCardSkeleton() {
   return (
     <div className="animate-pulse flex flex-col items-center">
-      <div className="w-full rounded-full bg-gray-600 circle" />
+      <div className="rounded-full h-40 w-40 md:h-[208px] md:w-[208px] bg-gray-600" />
       <div className="h-4 w-8/12 bg-gray-600 mt-2" />
-      <style jsx>{`
-        .circle::before {
-          content: '';
-          display: block;
-          padding-bottom: 100%;
-        }
-      `}</style>
     </div>
   );
 }
