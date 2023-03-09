da597b5056c2f01072055f2f090e2c284d0c3c19
Alexander Knipfer <alexanderknipfer@gmail.com>
2023-03-09 00:27:37 -0500

    Remove log

--- app/api/playlists/route.ts
+++ app/api/playlists/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from 'next/server';
 import { spotifyService } from '@/lib/spotifyService';
 
 export async function GET(request: Request) {
-  console.log('hello');
   const { searchParams } = new URL(request.url);
   const limit = searchParams.get('limit');
   const offset = searchParams.get('offset');
