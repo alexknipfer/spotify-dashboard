4ab62119dd8077b1c3b61d3fed59af12780d379f
Alexander Knipfer <alexanderknipfer@gmail.com>
2023-03-09 00:27:28 -0500

    Fix skeletons

--- app/dashboard/tracks/[id]/page.tsx
+++ app/dashboard/tracks/[id]/page.tsx
@@ -6,9 +6,12 @@ import { Fragment } from 'react';
 import { APIRoute } from '@/models/APIRoute.enum';
 import { SpotifyAudioFeatures, SpotifyTrack } from '@/models/Spotify';
 import TrackDetails from '@/components/TrackDetails';
-import HeadlineStatistic from '@/components/HeadlineStatistic';
+import HeadlineStatistic, {
+  HeadlineStatisticSkeleton,
+} from '@/components/HeadlineStatistic';
 import { millisToMinutesAndSeconds } from '@/lib/utils';
 import AudioFeaturesChart from '@/components/AudioFeaturesChart';
+import SkeletonList from '@/components/SkeletonList';
 
 interface Props {
   params: { id: string };
@@ -28,26 +31,31 @@ export default function Track({ params }: Props) {
     <Fragment>
       <TrackDetails isLoading={!data} track={data?.track} />
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 my-10">
-        <HeadlineStatistic
-          label="Popularity"
-          value={`${data?.track.popularity}%`}
-          isLoading={!data}
-        />
-        <HeadlineStatistic
-          label="Loudness"
-          value={`${data?.audioFeatures.loudness}`}
-          isLoading={!data}
-        />
-        <HeadlineStatistic
-          label="Tempo (BPM)"
-          value={`${Math.round(data?.audioFeatures.tempo || 0)}`}
-          isLoading={!data}
-        />
-        <HeadlineStatistic
-          label="Duration"
-          value={millisToMinutesAndSeconds(data?.track.duration_ms || 0)}
-          isLoading={!data}
-        />
+        {!data ? (
+          <SkeletonList
+            rows={4}
+            skeletonComponent={<HeadlineStatisticSkeleton />}
+          />
+        ) : (
+          <Fragment>
+            <HeadlineStatistic
+              label="Popularity"
+              value={`${data?.track.popularity}%`}
+            />
+            <HeadlineStatistic
+              label="Loudness"
+              value={`${data?.audioFeatures.loudness}`}
+            />
+            <HeadlineStatistic
+              label="Tempo (BPM)"
+              value={`${Math.round(data?.audioFeatures.tempo || 0)}`}
+            />
+            <HeadlineStatistic
+              label="Duration"
+              value={millisToMinutesAndSeconds(data?.track.duration_ms || 0)}
+            />
+          </Fragment>
+        )}
       </div>
       {data?.audioFeatures && (
         <AudioFeaturesChart audioFeatures={data.audioFeatures} />
