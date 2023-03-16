74be08e65e86132f38dffeeecf9d30039ce95162
Alexander Knipfer <alexanderknipfer@gmail.com>
2023-03-15 22:46:57 -0400

    Testing without query params

--- app/dashboard/tracks/(index)/page.tsx
+++ app/dashboard/tracks/(index)/page.tsx
@@ -1,20 +1,24 @@
 import { Fragment } from 'react';
 
-import { SpotifyTimeRange } from '@/models/Spotify';
+// import { SpotifyTimeRange } from '@/models/Spotify';
 import TrackCard from '@/components/TrackCard';
-import { isQueryParamValidSpotifyRange } from '@/lib/utils';
+// import { isQueryParamValidSpotifyRange } from '@/lib/utils';
 import { spotifyService } from '@/lib/spotify';
+import { SpotifyTimeRange } from '@/models/Spotify';
 
-interface Props {
-  searchParams: { [key: string]: string | string[] | undefined };
-}
+// interface Props {
+//   searchParams: { [key: string]: string | string[] | undefined };
+// }
 
-export default async function Tracks({ searchParams }: Props) {
-  const currentTimeRange = isQueryParamValidSpotifyRange(searchParams.range)
-    ? searchParams.range
-    : SpotifyTimeRange.LONG_TERM;
+export default async function Tracks() {
+  // const currentTimeRange = isQueryParamValidSpotifyRange(searchParams.range)
+  //   ? searchParams.range
+  //   : SpotifyTimeRange.LONG_TERM;
 
-  const data = await spotifyService.getTopTracks(20, currentTimeRange);
+  const data = await spotifyService.getTopTracks(
+    20,
+    SpotifyTimeRange.LONG_TERM,
+  );
 
   return (
     <Fragment>
