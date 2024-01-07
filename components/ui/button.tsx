70b76346238cde6c0d36c376979d81f1e707b2bb
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-06 22:07:14 -0500

    Add logout variant to button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
+import { signOut } from 'next-auth/react';
 
 import { cn } from '@/lib/utils';
 
@@ -19,12 +22,14 @@ const buttonVariants = cva(
           'bg-zinc-100 text-zinc-900 hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80',
         ghost: 'hover:text-zinc-900 dark:hover:text-zinc-50',
         link: 'text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50',
+        logout: 'justify-start py-1.5',
       },
       size: {
         default: 'h-10 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
+        logout: 'px-0 py-0',
       },
     },
     defaultVariants: {
@@ -48,6 +53,13 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+          if (variant === 'logout') {
+            signOut();
+          } else if (props.onClick) {
+            props.onClick(event);
+          }
+        }}
         {...props}
       />
     );
