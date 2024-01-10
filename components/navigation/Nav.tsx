ce539ed7fb603a599c6908a60581f0c768dc48bb
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-09 20:00:24 -0500

    Move nav components into navigation sub directory

---
+++ components/navigation/Nav.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
+import { navItems } from '@/lib/navItems';
+
+export default function Nav() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="hidden md:block space-x-4">
+      {Object.entries(navItems).map(([path, { name }]) => (
+        <Link
+          key={path}
+          href={path}
+          className={cn('text-zinc-50 font-medium text-sm', {
+            'text-zinc-400': pathname !== path,
+          })}
+        >
+          {name}
+        </Link>
+      ))}
+    </nav>
+  );
+}
