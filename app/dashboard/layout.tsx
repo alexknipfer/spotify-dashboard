27cd14dc2c74fb15a81fbae4b51b208233656e13
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-10 19:26:22 -0500

    Fix nav sticky position

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,9 +9,7 @@ interface Props {
 export default async function DashboardLayout({ children }: Props) {
   return (
     <Fragment>
-      <div className="border-b border-zinc-800">
-        <TopMenu />
-      </div>
+      <TopMenu />
       <main className="px-6 md:px-16 lg:px-28 pt-10 pb-20 max-w-screen-2xl mx-auto">
         {children}
       </main>
