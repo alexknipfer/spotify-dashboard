bcf3d0d28f7ee9cf864e4b900b742c08164e6a66
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-10 23:56:00 -0500

    Fix imports

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -3,8 +3,8 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
+import { navItems } from '@/config/nav';
 import { cn } from '@/lib/utils';
-import { navItems } from 'config/nav';
 
 export default function Nav() {
   const pathname = usePathname();
