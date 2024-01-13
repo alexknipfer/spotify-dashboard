2de44aeb4c3687d575a4b81f4074a5f812ed0f4c
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-13 16:17:41 -0500

    Fix border from cutting off on large screens

--- components/navigation/top-menu.tsx
+++ components/navigation/top-menu.tsx
@@ -10,19 +10,21 @@ import { RoutePath } from '@/types/route-path.enum';
 
 export default async function TopMenu() {
   return (
-    <header className="flex items-center justify-between h-16 sticky top-0 z-50 border-b border-zinc-800 backdrop-blur supports-[backdrop-filter]:bg-background/70 px-6 md:px-16 lg:px-28 max-w-screen-2xl mx-auto">
-      <div className="flex items-center">
-        <Link
-          href={RoutePath.DASHBOARD}
-          className="hidden md:block w-24 mr-6"
-          aria-label="Return to dashboard"
-        >
-          <SpotifyLogo />
-        </Link>
-        <MobileNavSheet />
-        <Nav />
+    <header className="h-16 sticky top-0 z-50 border-b border-zinc-800 backdrop-blur supports-[backdrop-filter]:bg-background/70">
+      <div className="flex items-center justify-between h-full px-6 md:px-16 lg:px-28 max-w-screen-2xl mx-auto">
+        <div className="flex items-center">
+          <Link
+            href={RoutePath.DASHBOARD}
+            className="hidden md:block w-24 mr-6"
+            aria-label="Return to dashboard"
+          >
+            <SpotifyLogo />
+          </Link>
+          <MobileNavSheet />
+          <Nav />
+        </div>
+        <NavProfileMenu />
       </div>
-      <NavProfileMenu />
     </header>
   );
 }
